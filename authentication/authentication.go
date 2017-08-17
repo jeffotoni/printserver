75cd65b7102e3e14d4645aba7fb8b3a5757378b5
jefferson otoni lima <jeff.otoni@gmail.com>
2017-08-17 20:34:01 -0300

    bug time.hours

--- authentication/authentication.go
+++ authentication/authentication.go
@@ -109,7 +109,7 @@ func GenerateJWT(model models.User) string {
 		StandardClaims: jwt.StandardClaims{
 
 			// Expires in 8 hours
-			ExpiresAt: time.Now().Add(time.Hour * ExpirationHours * DayExpiration).Unix(),
+			ExpiresAt: time.Now().Add(time.Hour * 24 * 14).Unix(),
 			Issuer:    ProjectTitle,
 		},
 	}
